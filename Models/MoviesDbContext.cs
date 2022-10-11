57587acad809181412605945796bb8ca3e9230ff
mckay-moulton <79293626+mckay-moulton@users.noreply.github.com>
2022-10-11 16:39:05 -0600

    Add project files.

---
+++ Models/MoviesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Magic.Models
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Movie> Movie_Db { get; set; }
+    }
+}
